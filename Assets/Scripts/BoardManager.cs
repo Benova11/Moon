6e7b74680a4a26f2a21d3da0231c85a5207c3305
ben <ben.afr@gmail.com>
2022-04-08 16:54:58 +0300

    Board logic

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -6,6 +6,7 @@ public class BoardManager : MonoBehaviour
   enum PlayerType { X, O }
   PlayerType currentPlayerType = PlayerType.X;
   int CurrentPlayerPieceValue { get { return (int)currentPlayerType; } }
+  int NumOfEmptyTiles { get { return currentBoardPieces.FindAll(x => x == -1).Count; } }
 
   [SerializeField] GameObject XPiecePrefab;
   [SerializeField] GameObject OPiecePrefab;
@@ -15,20 +16,50 @@ public class BoardManager : MonoBehaviour
 
   void Start()
   {
-    currentBoardPieces = new List<int> { -1,-1,-1,-1,-1,-1,-1,-1,-1};
+    currentBoardPieces = new List<int> { -1, -1, -1, -1, -1, -1, -1, -1, -1 };
   }
 
-  public GameObject GenarateNextBoardPiece(int gridIndex)
+  public void GenarateNextBoardPiece(int gridIndex)
   {
-    if (currentBoardPieces[gridIndex] != -1)
+    if (currentBoardPieces[gridIndex] != -1 || NumOfEmptyTiles == 0)
     {
-      Debug.Log("Place already taken");
-      return null; //place already taken
+      return; //place already taken or board full
     }
     GameObject piecePrefabToInstantiate = currentPlayerType == PlayerType.X ? XPiecePrefab : OPiecePrefab;
     currentBoardPieces[gridIndex] = CurrentPlayerPieceValue;
     currentPlayerType = (PlayerType)Mathf.Abs(1 - CurrentPlayerPieceValue);
-
-    return Instantiate(piecePrefabToInstantiate, boardContainer.transform.GetChild(gridIndex));
+    Instantiate(piecePrefabToInstantiate, boardContainer.transform.GetChild(gridIndex));
+    CheckBoardState();
   }
+
+  void CheckBoardState()
+  {
+    if (NumOfEmptyTiles > 5) return;
+
+    for (int i = 1; i <= 3; i++)
+    {
+      if (currentBoardPieces[i - 1] == (int)PlayerType.X)
+        Debug.Log("");
+      else if (currentBoardPieces[i - 1] == (int)PlayerType.O)
+        Debug.Log("");
+
+      if (currentBoardPieces[i*3 - 1] == (int)PlayerType.X)
+        Debug.Log("");
+      else if (currentBoardPieces[i*3 - 1] == (int)PlayerType.O)
+        Debug.Log("");
+
+      if (i < 3)
+      {
+        if (currentBoardPieces[i * 4 - 1] == (int)PlayerType.X)
+          Debug.Log("");
+        else if (currentBoardPieces[i * 4 - 1] == (int)PlayerType.O)
+          Debug.Log("");
+      }
+
+    }
+  }
+
+  //  if (currentBoardPieces[i * 1 - 1] == (int) PlayerType.X)
+  //else if (currentBoardPieces[i + 1] == (int) PlayerType.O)
+
 }
