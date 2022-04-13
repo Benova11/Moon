9a8f54f721572cec27e4403e691f4b419b8b0fe8
Benz <ben.afr@gmail.com>
2022-04-13 23:44:22 +0300

    MainMenu redesign

--- Assets/MainMenuManager.cs
+++ Assets/MainMenuManager.cs
@@ -1,15 +1,43 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
-  GameMode selectedMode = GameMode.PVP;
+  GameMode selectedGameMode = GameMode.PVP;
+  [SerializeField] CanvasGroup difficultyPanelCanvasGroup;
+  [SerializeField] Button[] gameModesButtonsArr;
+
+  void Start()
+  {
+    selectedGameMode = (GameMode)(1 - (int)selectedGameMode);
+    ToggleGameMode();
+  }
 
   public void StartGame()
   {
     SceneManager.LoadSceneAsync("GameScene").completed += (AsyncOperation obj) =>
-    { GameManager.Instance.StartGame(selectedMode); };
+    { GameManager.Instance.StartGame(selectedGameMode); };
   }
 
-  //public void 
+  public void ToggleGameMode()
+  {
+    difficultyPanelCanvasGroup.alpha = 1.3f - (float)selectedGameMode;
+    gameModesButtonsArr[(int)selectedGameMode].colors = GetGameModeButtonStyle(false);
+    selectedGameMode = (GameMode)(1 - (int)selectedGameMode);
+    gameModesButtonsArr[(int)selectedGameMode].colors = GetGameModeButtonStyle(true);
+  }
+
+  ColorBlock GetGameModeButtonStyle(bool isSelected)
+  {
+    ColorBlock colorBlock = new ColorBlock();
+    colorBlock.normalColor = new Color(1, 1, 1, isSelected ? 1f :0f);
+    colorBlock.highlightedColor = new Color(1, 1, 1, isSelected ? 1f : 0.5f);
+    colorBlock.pressedColor = new Color(0.8f, 0.8f, 0.8f, isSelected ? 1f : 0.5f);
+    colorBlock.selectedColor = colorBlock.normalColor;
+    colorBlock.disabledColor = new Color(1, 1, 1, 0);
+    colorBlock.colorMultiplier = 1;
+    colorBlock.fadeDuration = 0.2f;
+    return colorBlock;
+  }
 }
