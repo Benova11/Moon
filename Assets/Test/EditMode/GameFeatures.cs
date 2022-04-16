17a65fc536261c19a3a8843fa75312dad6173102
Benz <ben.afr@gmail.com>
2022-04-16 23:45:33 +0300

    Added test environment and assemblies references

---
+++ Assets/Test/EditMode/GameFeatures.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    public class GameFeatures
+    {
+    int x = 0;
+        [Test]
+        public void GameFeaturesSimplePasses()
+        {
+      Assert.AreEqual(x, 0);
+        }
+    }
+}
